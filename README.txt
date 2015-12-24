b5f2f99909fad788d94176413a0b59347976fb65
Purple-6 <satheeshkumar.six@gmail.com>
2015-12-24 19:38:38 +0530

    satheesh module

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+INSTALLATION
+============
+1. Install the Simplelogin module the usual way at admin/build/modules.
+2. Configure the background image admin/config/simplelogin/loginbg
---
+++ templates/page--simplelogin.tpl.php
@@ -0,0 +1,33 @@
+<?php $path = drupal_get_path_alias(); ?>
+
+<div class="loginregis">
+  
+  <?php if(($path == 'user/password') || ($path == 'user/register')): ?>
+    <?php print l('Login-In', 'user/login', array('attributes' => array('class' => array('signup')))); ?>
+  <?php else: ?>
+    <?php if (variable_get('user_register')): ?>
+      <?php print l('Sign-up', 'user/register', array('attributes' => array('class' => array('signup')))); ?>
+    <?php endif; ?>
+  <?php endif; ?>
+
+	<div class="ex-center-position">		
+
+	  <?php if (isset($logo)): ?> 
+	    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="login-logo">
+	      <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" />
+	    </a>
+	  <?php else: ?>  
+	    <h3><?php print $site_name; ?></h3>
+	  <?php endif; ?>
+
+	  <?php print $messages; ?>
+	
+		<?php print render($page['content']); ?>
+		
+		<?php if(($path == 'user') || ($path == 'user/login')): ?>
+			<p>Forgot password? <?php print l('Click here', 'user/password'); ?></p> 
+		<?php endif; ?>
+
+	</div>
+
+</div>
