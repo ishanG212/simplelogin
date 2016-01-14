5e7452fdc931f20354a7e7625b0f1da19f64c7a1
Purple-6 <satheeshkumar.six@gmail.com>
2016-01-14 10:33:11 +0530

    whitepage error menu_execute_active_handler()

--- templates/page--simplelogin.tpl.php
+++ templates/page--simplelogin.tpl.php
@@ -3,18 +3,10 @@
  * @file
  * Customize login page HTML structure.
  */
- $path = drupal_get_path_alias();
- $variable = variable_get('simplelogin_fid', '');
-   if ($variable) {
-     $file = file_load($variable);
-     $bgimg = file_create_url($file->uri);
-     $css = "body.simplelogin {background-image: url('$bgimg') }";
-     drupal_add_css($css, 'inline');
-   }
 ?>
 <div class="loginregis">  
   <?php if(($path == 'user/password') || ($path == 'user/register')): ?>
-    <?php print l(t('Login-In'), 'user/login', array('attributes' => array('class' => array('signup')))); ?>
+    <?php print l(t('Login'), 'user/login', array('attributes' => array('class' => array('signup')))); ?>
   <?php else: ?>
     <?php if (variable_get('user_register')): ?>
       <?php print l(t('Sign-up'), 'user/register', array('attributes' => array('class' => array('signup')))); ?>
@@ -31,7 +23,7 @@
   	<?php print $messages; ?>
   	<?php print render($page['content']); ?>
   	<?php if(($path == 'user') || ($path == 'user/login')): ?>
-  	  <p>Forgot password? <?php print l(t('Click here'), 'user/password'); ?></p>
+  	  <p><?php print t('Forgot password?'); ?> <?php print l(t('Click here'), 'user/password'); ?></p>
   	<?php endif; ?>
   </div>
 </div>
